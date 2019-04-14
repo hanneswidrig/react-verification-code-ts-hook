c97b466239fe5895318e300ba7386eaabb59247a
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-04-13 22:55:35 -0400

    Improved refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import * as React from 'react'
 import { VerificationCode } from './VerificationCode'
 
-export const App = () => {
-	return (
-		<div>
-			<VerificationCode />
-		</div>
-	)
-}
+export const App = () => <VerificationCode />
